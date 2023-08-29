00c56e1dd2705413e9eb0834d138914cc7beaf20
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 <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>
2023-08-29 18:27:03 +0300

    First commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
@@ -7,18 +8,135 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://voicu:Irefutabil1986@cluster0.ty95ttd.mongodb.net/todolistDB")
+
+// index
+
+const itemsSchema = {
+  name: String
+}
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your todolist"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+
+const defaultItems = [item1, item2];
+
+// end of index
+
+
+//Work
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+const list1 = new List ({
+  name: "Add your task"
+});
+
+const list2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+const defaultList = [list1, list2];
+
+// End of work
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  Item.find({}).then(function(FoundItems){
+    
+    if(FoundItems.length === 0) {
+      Item.insertMany(defaultItems).then(function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Succes")
+        }
+      });
+      res.redirect("/")
+    } else {
+      res.render("index.ejs", {newItems: FoundItems});
+    }
+
   });
+  
+});
 
   app.get("/work", (req, res) => {
-    res.render("work.ejs");
+    List.find({}).then(function(FoundItems){
+    
+      if(FoundItems.length === 0) {
+        List.insertMany(defaultList).then(function(err) {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log("Succes")
+          }
+        });
+        res.redirect("/work")
+      } else {
+        res.render("work.ejs", {workItems: FoundItems});
+      }
+  
+    });
   });
 
-  // app.post("submit", (req, res) => {
-  //   res.render("index.ejs")
-  // })
+
+  app.post("/", (req, res) => {
+    const newItem = req.body.newItem;
+    const items = new Item ({
+      name: newItem
+    })
+    defaultItems.push(items)
+    items.save();
+    res.redirect("/")
+  });
+
+  app.post("/work", (req, res) => {
+    const newItem = req.body.newItem;
+    const item = new List ({
+      name: newItem
+    })
+    defaultList.push(item)
+    item.save();
+    res.redirect("/work")
+  });
+
+  app.post("/delete", (req, res) => {
+    const deleteItem = req.body.delete;
+    Item.findByIdAndRemove(deleteItem)
+.then(function () {
+    console.log("Successfully removed");
+})
+.catch(function (err) {
+    console.log(err);
+});
+res.redirect("/");
+  })
+
+
+  app.post("/delete2", (req, res) => {
+    const deleteItem = req.body.delete;
+    List.findByIdAndRemove(deleteItem)
+.then(function () {
+    console.log("Successfully removed");
+})
+.catch(function (err) {
+    console.log(err);
+});
+res.redirect("/work");
+  })
 
 
   app.listen(port, () => {
